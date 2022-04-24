c4bbc069b1f2243c60d5816a1cf38e400f665929
muranakar <p1211065@gmail.com>
2022-04-24 10:38:52 +0900

    Labelの表示非表示を実装

--- RandomMixedJuice/ViewController.swift
+++ RandomMixedJuice/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     @IBOutlet weak private var fruitCheckButton3: UIButton!
     @IBOutlet weak private var fruitCheckButton4: UIButton!
     @IBOutlet weak private var fruitCheckButton5: UIButton!
+    @IBOutlet weak private var startButton: UIButton!
+    @IBOutlet weak private var stopButton: UIButton!
 
     var fruitCheckButtons: [UIButton] {
         [
@@ -33,11 +35,34 @@ class ViewController: UIViewController {
             fruitCheckButton5
         ]
     }
+
+    var resultFruitLabels: [UILabel] {
+        [
+            resultFruit1Label,
+            resultFruit2Label,
+            resultFruit3Label,
+            resultFruit4Label,
+            resultFruit5Label
+        ]
+    }
+
+    var resultFruits: [String] {
+        [
+            resultFruit1,
+            resultFruit2,
+            resultFruit3,
+            resultFruit4,
+            resultFruit5
+        ]
+    }
     var fruitIsChecks: [Bool] = [false, false, false, false, false]
     var fruitCheckArrayIndex: [Int] = Array(0...4)
 
-    private var fruitButtonsNumberDictionary: [UIButton: Int] {
-        [UIButton: Int](uniqueKeysWithValues: zip(fruitCheckButtons, fruitCheckArrayIndex))
+    private var fruitButtonsNumberDictionary: [UIButton: String] {
+        [UIButton: String](uniqueKeysWithValues: zip(fruitCheckButtons, resultFruits))
+    }
+    private var fruitButtonsAndResultLabelDictionary: [UIButton: UILabel] {
+        [UIButton: UILabel](uniqueKeysWithValues: zip(fruitCheckButtons, resultFruitLabels))
     }
 
     private var fruitsLines: [String] = []
@@ -46,11 +71,15 @@ class ViewController: UIViewController {
     private var resultFruit3: String = ""
     private var resultFruit4: String = ""
     private var resultFruit5: String = ""
-    var resultsFruit: [String] = []
+    var allResultFruits: [String] = []
+    var resultFruitsIsSelected: [String] = []
+
 
     var btnTimer: Timer!
 
     var randomState: RandomState = .stop
+
+    var isCheckCount: Int = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,47 +93,75 @@ class ViewController: UIViewController {
         let newCVSLines = csvLines.filter { $0 != "" }
         fruitsLines = newCVSLines
     }
-    @objc func changefruit() {
+
+    @IBAction func save(_ sender: Any) {
+        //選択されたボタン
+
+        //選択されたボタンから、Resultの値を取得
+
+        //Resultの値を、配列に追加
+
+        //その値を保存する。
+
+        //履歴を閲覧するボタンは別のボタンで画面遷移
+
+        resultFruitsIsSelected
+    }
+
+    @IBAction private func randomStart(_ sender: Any) {
         randomState = .start
+        configueButtonIsEnable()
         switch randomState {
         case .start:
-            var count = 1
-            resultsFruit = []
-            while count < 6 {
-                let randomFruit = fruitsLines.randomElement()!
-                let filterResultsFruit = resultsFruit.filter { randomFruit == $0 }
-                if filterResultsFruit.isEmpty {
-                    resultsFruit.append(randomFruit)
-                    count += 1
-                }
-            }
-            resultFruit1 = resultsFruit[0]
-            resultFruit2 = resultsFruit[1]
-            resultFruit3 = resultsFruit[2]
-            resultFruit4 = resultsFruit[3]
-            resultFruit5 = resultsFruit[4]
-            configureViewLabel()
+            self.btnTimer = Timer.scheduledTimer(
+                timeInterval: 0.1,
+                target: self,
+                selector: #selector(self.changefruit),
+                userInfo: nil, repeats: true
+            )
         case .stop:
             break
         }
     }
 
-    @IBAction private func randomStart(_ sender: Any) {
-        self.btnTimer = Timer.scheduledTimer(
-            timeInterval: 0.1,
-            target: self,
-            selector: #selector(self.changefruit),
-            userInfo: nil, repeats: true
-        )
+    @objc func changefruit() {
+        var count = 1
+        allResultFruits = []
+        while count < 6 {
+            let randomFruit = fruitsLines.randomElement()!
+            let filterResultsFruit = allResultFruits.filter { randomFruit == $0 }
+            if filterResultsFruit.isEmpty {
+                allResultFruits.append(randomFruit)
+                count += 1
+            }
+        }
+        resultFruit1 = allResultFruits[0]
+        resultFruit2 = allResultFruits[1]
+        resultFruit3 = allResultFruits[2]
+        resultFruit4 = allResultFruits[3]
+        resultFruit5 = allResultFruits[4]
+        configureViewLabel()
     }
 
     @IBAction private func randomStop(_ sender: Any) {
-        self.btnTimer!.invalidate()
-        configureViewLabel()
+        randomState = .stop
+        configueButtonIsEnable()
+        switch randomState {
+        case .start:
+            break
+        case .stop:
+            self.btnTimer!.invalidate()
+            configureViewLabel()
+        }
     }
 
     @IBAction private func fruitCheckButton(_ sender: UIButton) {
         sender.isSelected.toggle()
+        if !sender.isSelected {
+            fruitButtonsAndResultLabelDictionary[sender]?.isHidden = true
+        } else {
+            fruitButtonsAndResultLabelDictionary[sender]?.isHidden = false
+        }
     }
     private func configureViewLabel() {
         resultFruit1Label.text = resultFruit1
@@ -116,8 +173,19 @@ class ViewController: UIViewController {
 
     private func configureViewButton() {
         fruitCheckButtons.forEach { uiButton in
+            uiButton.isSelected = true
             uiButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
             uiButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .selected)
+        }
+    }
+    private func configueButtonIsEnable() {
+        switch randomState {
+        case .start:
+            startButton.isEnabled = false
+            stopButton.isEnabled = true
+        case .stop:
+            startButton.isEnabled = true
+            stopButton.isEnabled = false
         }
     }
 }
